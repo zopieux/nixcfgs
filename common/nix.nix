aedc794dd6b544998832d4dd82ed5ef95a92028a
Alexandre Macabies <web+oss@zopieux.com>
2023-06-09 20:35:24 +0200

    Add alex to trusted-users.

--- common/nix.nix
+++ common/nix.nix
@@ -5,6 +5,9 @@ in {
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Substituters.
+  nix.settings.trusted-users = [ "root" "alex" ];
+
   # Set default nixpkgs to the pinned flake-installed version.
   environment.etc.nixpkgs.source = lib.cleanSource pkgs.path;
   nix.nixPath = [ "nixpkgs=/etc/nixpkgs" ];
