036741a7669c5ab36fdb99fa7fae1a79337e1ee2
Alexandre Macabies <web+oss@zopieux.com>
2023-06-24 18:46:10 +0200

    Add ntfs support.

--- machines/desktop2k/hardware.nix
+++ machines/desktop2k/hardware.nix
@@ -3,6 +3,7 @@
 rec {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  boot.supportedFilesystems = [ "ntfs" ];
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
